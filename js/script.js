ec2faed1875ec30d48c7fabddcfc6f3ab71df140
opeksmansaku <opeksmansaku@gmail.com>
2026-06-20 01:43:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -234,3 +234,34 @@ const hitungCountdown = () => {
 hitungCountdown();
 
 const timer = setInterval(hitungCountdown,1000);
+// =======================
+// SCROLL TO TOP
+// =======================
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        scrollBtn.classList.add("show");
+
+    }else{
+
+        scrollBtn.classList.remove("show");
+
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
